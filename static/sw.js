11b3f43772ab868c3535680b9d9ce21323aeab46
Mod617 <hounvosegbede@gmail.com>
2026-08-08 20:09:44 +0100

    Ajout PWA (manifest + service worker) et correction affichage images plats

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,15 @@
-self.addEventListener("push", function(event){
+const CACHE_NAME = 'wheke-food-v1';
 
-self.registration.showNotification("Nouvelle commande",{
-body:"Un client vient de commander"
-})
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
 
-})
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
